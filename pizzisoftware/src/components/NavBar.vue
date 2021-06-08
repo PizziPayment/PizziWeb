e237ce28c88348b12d87de45e715a2bebb364e30
Simon HOULIAT <simon@macbook-pro-de-simon.home>
2021-06-08 20:05:10 +0200

    feature: dashboard layout

---
+++ pizzisoftware/src/components/NavBar.vue
@@ -0,0 +1,68 @@
+<template>
+  <nav>
+    <v-app-bar flat color="transparent">
+      <v-app-bar-nav-icon color="white" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="white--text">
+        <span class="font-weight-bold">Pizzi</span>
+        <span class="font-weight-light">Dashboard</span>
+      </v-toolbar-title>
+      <v-spacer/>
+      <v-btn text color="white">
+        <span>Sign Out</span>
+        <v-icon medium right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app color="#3a474e">
+      <v-row align-center>
+        <v-col class="mt-6">
+          <v-avatar color="primary" size="100">
+            <v-icon>mdi-content-cut</v-icon>
+          </v-avatar>
+          <p class="white--text sub-heading mt-6 font-weight-bold">
+            Adult 'Hair
+          </p>
+        </v-col>
+      </v-row>
+      <v-list class="mt-6">
+        <v-list-item v-for="(link, i) in links" :key="i" @click="link.action">
+          <v-list-item-icon>
+            <v-icon color="rgb(112, 192, 242)" v-text="link.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: false,
+        links: [
+          { icon: 'mdi-plus', text: 'Add Widget', action: this.addWidget},
+          { icon: 'mdi-account', text: 'Account', action: this.toAccountPage},
+          { icon: 'mdi-cog', text: 'Settings', action: this.toSettingsPage},
+        ]
+      }
+    },
+    methods: {
+      addWidget() {
+        console.log('addWidget')
+      },
+      toAccountPage() {
+        console.log('toAccountPage')
+      },
+      toSettingsPage() {
+        console.log('toSettingsPage')
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ pizzisoftware/src/views/Dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fluid class="background">
+        <Navbar />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Navbar from '@/components/NavBar.vue'
+
+
+export default {
+  components : { Navbar },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+  .background {
+    background-color: rgb(40, 50, 55);
+    height: 100%;
+  }
+</style>
