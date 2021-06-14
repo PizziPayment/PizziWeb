5de4f67c59d0dc732fe80ed0a34a1e5c23778f4b
remipoulenard <remi.poulenard@epitech.eu>
2021-06-14 21:27:19 +0200

    fix: widgets positions

--- pizzisoftware/src/views/Dashboard.vue
+++ pizzisoftware/src/views/Dashboard.vue
@@ -3,14 +3,6 @@
     <v-content>
       <v-container fluid class="background">
         <navbar />
-        <v-row class="pa-6">
-          <v-col class="pa-6">
-            <sales />
-          </v-col>
-          <v-col class="pa-6">
-            <graphs />
-          </v-col>
-        </v-row>
         <v-row class="pa-6 d-flex align-center">
           <v-col class="pa-6">
             <cashPayment />
@@ -19,12 +11,20 @@
             <productReturn />
           </v-col>
         </v-row>
+        <v-row class="pa-6">
+          <v-col class="pa-6">
+            <sales />
+          </v-col>
+          <v-col class="pa-6">
+            <graphs />
+          </v-col>
+        </v-row>
         <!-- if no widget chosen -->
         <!-- <v-row align="center" justify="center" class="fill-height pb-10">
           <v-col>
             <h1 class="display-4 blue-grey--text text--lighten-4">Welcome Fredo</h1>
             <p class="headline blue-grey--text text--lighten-4">Please select widget(s) you want to work with</p> -->
-          <!-- </v-col>
+        <!-- </v-col>
         </v-row> -->
         <!-- -->
       </v-container>
@@ -33,26 +33,24 @@
 </template>
 
 <script>
-import navbar from '@/components/NavBar.vue'
+import navbar from "@/components/NavBar.vue";
 // to remove when widget manager finished
-import sales from '@/components/widgets/Sales.vue'
-import productReturn from '@/components/widgets/ProductReturn.vue'
-import cashPayment from '@/components/widgets/CashPayment/CashPayment.vue'
-import graphs from '@/components/widgets/Graphs.vue'
+import sales from "@/components/widgets/Sales.vue";
+import productReturn from "@/components/widgets/ProductReturn.vue";
+import cashPayment from "@/components/widgets/CashPayment/CashPayment.vue";
+import graphs from "@/components/widgets/Graphs.vue";
 
 export default {
-  components : { navbar, sales, productReturn, cashPayment, graphs },
-  data () {
-    return {
-
-    }
-  }
-}
+  components: { navbar, sales, productReturn, cashPayment, graphs },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
-  .background {
-    background-color: rgb(40, 50, 55);
-    height: 100%;
-  }
+.background {
+  background-color: rgb(40, 50, 55);
+  height: 100%;
+}
 </style>
