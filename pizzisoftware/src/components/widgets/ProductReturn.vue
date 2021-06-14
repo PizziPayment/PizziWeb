81d7bc4d456f39af8099bd0a4129f10bf149284a
remipoulenard <remi.poulenard@epitech.eu>
2021-06-14 21:27:06 +0200

    fix: v-model to store pizzi id

--- pizzisoftware/src/components/widgets/ProductReturn.vue
+++ pizzisoftware/src/components/widgets/ProductReturn.vue
@@ -1,20 +1,39 @@
 <template>
   <v-card color="transparent" elevation="0">
     <div class="white--text">
-      <h2 class="d-flex text-left">
-        Product Return
-      </h2>
+      <h2 class="d-flex text-right">Returned product</h2>
       <p class="subtitle-2 d-flex text-left ml-6">
-        Enter Pizzi to return product
+        Enter Pizzi ID to return product in your stock
       </p>
     </div>
-    <v-text-field 
-      label="Pizzi ID" 
-      outlined 
-      color="rgb(112, 192, 242)" 
+    <v-text-field
+      label="Pizzi ID"
+      outlined
+      v-model="pizziId"
+      color="rgb(112, 192, 242)"
       dark
       append-icon="mdi-undo"
-      prefix="#">
+      prefix="#"
+      @keyup.enter="returnProduct()"
+      @click="returnProduct()"
+    >
     </v-text-field>
   </v-card>
 </template>
+
+
+<script>
+export default {
+  props: ["value"],
+  data() {
+    return {
+      pizziId: null,
+    };
+  },
+  methods: {
+    returnProduct() {
+      console.log("id of product:", this.pizziId);
+    },
+  },
+};
+</script>
