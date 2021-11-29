27e89ff7947abd5d29e694aec48b99889f78aefe
Remi Poulenard <remipoulenard@Remis-MBP.home>
2021-11-28 16:09:56 -0800

    feat: store user tokens

--- pizzisoftware/src/store/default/actions.js
+++ pizzisoftware/src/store/default/actions.js
@@ -1,14 +1,24 @@
 // import Vue from 'vue'
 
-export async function userLogin ({ commit }, payload) {
-    if (payload) {
-        commit('UPDATE_ACCESS_TOKEN', payload.accessToken)
-        commit('UPDATE_REFRESH_TOKEN', payload.refreshToken)
-        commit('UPDATE_EXPIRATION_TOKEN', payload.expirationToken)
-        commit('UPDATE_USER_LOGGED_IN', 1)
-    }
+export async function userLogin({ commit }, payload) {
+  try {
+    console.log("payload", payload);
+    commit("UPDATE_ACCESS_TOKEN", payload.accessToken);
+    commit("UPDATE_REFRESH_TOKEN", payload.refreshToken);
+    commit("UPDATE_EXPIRATION_TOKEN", payload.expirationToken);
+    commit("UPDATE_USER_LOGGED_IN", 1);
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
 }
 
-export async function userLogout ({ commit }) {
-    commit('UPDATE_USER_LOGGED_IN', 0)
+export async function userLogout({ commit }) {
+  commit("UPDATE_USER_LOGGED_IN", 0);
+  // clear tokens
+  commit("UPDATE_ACCESS_TOKEN", null);
+  commit("UPDATE_REFRESH_TOKEN", null);
+  commit("UPDATE_EXPIRATION_TOKEN", null);
+  commit("UPDATE_USER_LOGGED_IN", 1);
 }
