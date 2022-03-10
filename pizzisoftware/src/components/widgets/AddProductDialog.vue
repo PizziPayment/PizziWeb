92bd0cb2de2c694cc365c8433f186ea0f9674dda
Remi Poulenard <remi.poulenard@epitech.eu>
2022-03-09 19:46:43 -0800

    feat: register product

---
+++ pizzisoftware/src/components/widgets/AddProductDialog.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="500" @click:outside="closeDialog()">
+      <v-card elevation="5">
+        <v-card-title class="text-h6 green darken-3">
+          Add product
+        </v-card-title>
+
+        <v-card-text class="mt-6">
+          <v-container>
+            <v-row>
+              <v-text-field
+                v-model="name"
+                label="Product name"
+                filled
+                clearable
+              ></v-text-field>
+            </v-row>
+            <v-row>
+              <v-text-field
+                v-model="price"
+                label="Price"
+                filled
+                type="number"
+                clearable
+              ></v-text-field>
+            </v-row>
+          </v-container>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green" text @click="saveItem()">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-snackbar color="green" v-model="snackbar" :timeout="1500">
+        <div class="text-center">
+          Item saved
+        </div>
+      </v-snackbar>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { mapGetters } from "vuex";
+
+export default {
+  data() {
+    return {
+      snackbar: false,
+      name: "",
+      price: "",
+      dialog: false,
+    };
+  },
+
+  computed: {
+    ...mapGetters("defaultStore", ["getAccessToken"]),
+  },
+
+  methods: {
+    show() {
+      this.dialog = true;
+    },
+
+    closeDialog() {
+      this.dialog = false;
+    },
+
+    clearItem() {
+      this.name = "";
+      this.price = "";
+    },
+
+    openSnackbar() {
+      this.snackbar = true
+    },
+
+    async saveItem() {
+      const bearerAuth = {
+        Authorization: "Bearer " + this.getAccessToken,
+      };
+      const body = {
+        items: [
+          {
+            name: this.name,
+            price: this.price,
+          },
+        ],
+      };
+      axios
+        .post(process.env.VUE_APP_RESOURCE_URL + "/shops/me/items", body, {
+          headers: bearerAuth,
+        })
+        .then((response) => {
+          console.log(response);
+          this.openSnackbar()
+          this.clearItem();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
--- pizzisoftware/src/views/RegisterProduct.vue
+++ pizzisoftware/src/views/RegisterProduct.vue
@@ -1,130 +1,232 @@
 <template>
   <v-app>
     <v-container fluid class="background">
-      <v-row justify="center" class="mt-12">
-        <v-col cols="12" md="8">
-          <material-card>
-            <template v-slot:heading>
-              <div class="display-2 font-weight-light">Add a new product</div>
-              <div class="subtitle-1 font-weight-light">
-                Add a new product to your inventory
-              </div>
-            </template>
-            <v-form  ref="form" v-model="valid" lazy-validation>
-              <v-container class="py-0">
-                <v-row>
-                  <v-col>
-                    <v-text-field
-                      clearable
-                      v-model="nameInput"
-                      :rules="nameRules"
-                      label="Name"
-                      required
-                    />
-                  </v-col>
-                  <v-col>
-                    <v-text-field
-                      clearable
-                      v-model="priceInput"
-                      :rules="priceRules"
-                      label="Price"
-                      required
-                    />
-                  </v-col>
-                  <v-col>
-                    <v-text-field
-                      clearable
-                      v-model="idInput"
-                      :rules="idRules"
-                      label="ID"
-                      required
-                    />
-                  </v-col>
-
-                  <v-col cols="12" class="text-center">
-                    <v-btn :disabled="!valid" color="success" @click='validate'>
-                      Add to Inventory
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-form>
-          </material-card>
-        </v-col>
-      </v-row>
-      <v-card class="mt-16 mx-12 pa-4">
-        <v-simple-table fixed-header height="300px">
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-left">
-                  Name
-                </th>
-                <th class="text-left">
-                  Price
-                </th>
-                <th class="text-left">
-                  ID
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="item in inventory"
-                :key="item.name"
-              >
-                <td>{{ item.name }}</td>
-                <td>{{ item.price }}€</td>
-                <td>#{{item.id}}</td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
+      <v-card class="mt-4 mx-2 pa-4">
+        <v-card-title>
+          <v-text-field
+            v-model="search"
+            prepend-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+            clearable
+          ></v-text-field>
+          <v-btn @click.stop="openAddProduct()" class="mx-3" color="primary"> add </v-btn>
+        </v-card-title>
+        <v-data-table
+          class="itemsTable"
+          :headers="headers"
+          :items="itemsData"
+          multisort
+          pagination.sync="pagination"
+          item-key="id"
+          loading="true"
+          :items-per-page="10"
+          :search="search"
+          :footer-props="{
+            showFirstLastPage: true,
+            firstIcon: 'mdi-arrow-collapse-left',
+            lastIcon: 'mdi-arrow-collapse-right',
+            prevIcon: 'mdi-minus',
+            nextIcon: 'mdi-plus'
+          }"
+        >
+        </v-data-table>
       </v-card>
+
+      <AddProductDialog ref="AddProductRef"/>
     </v-container>
   </v-app>
 </template>
 
 <script>
-import materialCard from "@/components/MaterialCard.vue";
+import AddProductDialog from "@/components/widgets/AddProductDialog.vue";
+import moment from 'moment'
 
 export default {
-  components: { materialCard },
+  components: { AddProductDialog },
 
   data: () => ({
+    search: '',
     valid: true,
-    nameInput: '',
+    nameInput: "",
     nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 20) || 'Name must be less than 20 characters',
-      ],
-    priceInput: '',
-    priceRules: [
-      v => !!v || 'Price is required',
-
+      (v) => !!v || "Name is required",
+      (v) => (v && v.length <= 20) || "Name must be less than 20 characters",
     ],
-    idInput: '',
-    idRules: [
-        v => !!v || 'Id is required',
-        v => (v && v.length == 5) || 'ID must be 5 characters long',
-      ],
+    priceInput: "",
+    priceRules: [(v) => !!v || "Price is required"],
     inventory: [],
+    headers: [
+      {
+        text: 'ID',
+        align: 'start',
+        filterable: false,
+        value: 'id',
+      },
+      { text: 'Item', value: 'name' },
+      { text: 'Price', value: 'price', sortable: true },
+      { text: 'Added date', value: 'addedDate' }
+    ],
+    itemsData: [
+      {
+        id: 1,
+        name: "Shampoing",
+        price: 24.99,
+        addedDate: moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+    {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+    {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+    {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+      {
+        id: 2,
+        name: "Coupe femme",
+        price: 35,
+        addedDate:  moment().format('LL')
+      },
+    ],
   }),
   methods: {
-    validate () {
-       if (this.$refs.form.validate()) {
-         const obj = {name: this.nameInput, price: this.priceInput, id : this.idInput}
-         this.inventory.push(obj)
-         this.$refs.form.reset()
-       }
-       this.$refs.form.reset()
-      },
-  }
+    openAddProduct () {
+      if (this.$refs.AddProductRef) {
+        this.$refs.AddProductRef.show()
+      }
+    },
+  },
 };
 </script>
 
 <style>
 .background {
   height: 100%;
+  width: 100%;
+}
+
+.itemsTable {
+  max-height: 80vh;
+  overflow: auto;
 }
 </style>
